0d03a4ed381a4150434ee261aaba6e4f63009682
omarmohammadfhd <143061584+omarmohammadfhd@users.noreply.github.com>
2024-11-11 11:18:30 -0800

    first commit

---
+++ object.py
@@ -0,0 +1,4 @@
+class Cup:
+    def __init__(self, color, goal):
+        self.color = color.strip().lower()
+        self.goal = goal
---
+++ puzzle.py
@@ -0,0 +1,194 @@
+
+from object import Cup
+
+
+
+cup1 = Cup('Black', 0)
+cup2 = Cup('Black', 0)
+cup3 = Cup('Black', 0)
+cup4 = Cup('Black', 0)
+cup5 = Cup('Black', 0)
+cup6 = Cup('Black', 0)
+cup7 = Cup('Black', 0)
+cup8 = Cup('Black', 0)
+cup9 = Cup('Black', 0)
+cup10 = Cup('white', 0)
+cup11 = Cup('white', 0)
+cup12 = Cup('Black', 0)
+cup13 = Cup('white', 0)
+cup14 = Cup('blue', 0)
+cup15 = Cup('Black', 0)
+cup16 = Cup('Black', 0)
+cup17 = Cup('Black', 0)
+cup18 = Cup('red', 0)
+cup19 = Cup('white', 0)
+cup20 = Cup('white', 0)
+cup21 = Cup('white', 0)
+cup22 = Cup('white', 0)
+cup23 = Cup('white', 0)
+cup24 = Cup('Black', 0)
+cup25 = Cup('Black', 0)
+cup26 = Cup('white', 0)
+cup27 = Cup('white', 0)
+cup28 = Cup('white', 0)
+cup29 = Cup('white', 0)
+cup30 = Cup('white', 0)
+cup31 = Cup('white', 0)
+cup32 = Cup('Black', 0)
+cup33 = Cup('Black', 0)
+cup34 = Cup('white', 0)
+cup35 = Cup('Black', 0)
+cup36 = Cup('Black', 0)
+cup37 = Cup('red', 1)
+cup38 = Cup('Black', 0)
+cup39 = Cup('Black', 0)
+cup40 = Cup('Black', 0)
+cup41 = Cup('white', 0)
+cup42 = Cup('blue', 1)
+cup43 = Cup('white', 0)
+cup44 = Cup('Black', 0)
+cup45 = Cup('Black', 0)
+cup46 = Cup('Black', 0)
+cup47 = Cup('white', 0)
+cup48 = Cup('white', 0)
+
+
+puzzle = [
+    [cup1, cup2, cup3, cup4, cup5, cup6, cup7, cup8],
+    [cup9, cup10, cup11, cup12, cup13, cup14, cup15, cup16],
+    [cup17, cup18, cup19, cup20, cup21, cup22, cup23, cup24],
+    [cup25, cup26, cup27, cup28, cup29, cup30, cup31, cup32],
+    [cup33, cup34, cup35, cup36, cup37, cup38, cup39, cup40],
+    [cup41, cup42, cup43, cup44, cup45, cup46, cup47, cup48]
+]
+
+
+
+#
+# cup1 = Cup('Black', 0)
+# cup2 = Cup('Black', 0)
+# cup3 = Cup('Black', 0)
+# cup4 = Cup('Black', 0)
+# cup5 = Cup('Black', 0)
+# cup6 = Cup('Black', 0)
+# cup7 = Cup('Black', 0)
+# cup8 = Cup('Black', 0)
+# cup9 = Cup('Black', 0)
+# cup10 = Cup('Black', 0)
+# cup11 = Cup('red', 1)
+# cup12 = Cup('white', 0)
+# cup13 = Cup('white', 0)
+# cup14 = Cup('white', 0)
+# cup15 = Cup('Black', 0)
+# cup16 = Cup('Black', 0)
+# cup17 = Cup('Black', 0)
+# cup18 = Cup('Black', 0)
+# cup19 = Cup('Black', 0)
+# cup20 = Cup('white', 0)
+# cup21 = Cup('white', 0)
+# cup22 = Cup('blue', 1)
+# cup23 = Cup('white', 0)
+# cup24 = Cup('white', 0)
+# cup25 = Cup('Black', 0)
+# cup26 = Cup('Black', 0)
+# cup27 = Cup('Black', 0)
+# cup28 = Cup('Black', 0)
+# cup29 = Cup('white', 0)
+# cup30 = Cup('white', 0)
+# cup31 = Cup('white', 0)
+# cup32 = Cup('white', 0)
+# cup33 = Cup('white', 0)
+# cup34 = Cup('blue', 0)
+# cup35 = Cup('red', 0)
+# cup36 = Cup('Black', 0)
+# cup37 = Cup('Black', 0)
+# cup38 = Cup('Black', 0)
+# cup39 = Cup('Black', 0)
+# cup40 = Cup('Black', 0)
+# cup41 = Cup('Black', 0)
+# cup42 = Cup('Black', 1)
+# cup43 = Cup('Black', 0)
+# cup44 = Cup('Black', 0)
+# cup45 = Cup('Black', 0)
+# # cup46 = Cup('Black', 0)
+# # cup47 = Cup('Black', 0)
+# # cup48 = Cup('Black', 0)
+#
+# puzzle = [
+#     [cup1, cup2, cup3, cup4, cup5, cup6, cup7, cup8,cup9],
+#     [ cup10, cup11, cup12, cup13, cup14, cup15, cup16,cup17,cup18],
+#     [ cup19, cup20, cup21, cup22, cup23, cup24,cup25, cup26, cup27],
+#     [ cup28, cup29, cup30, cup31, cup32,cup33, cup34, cup35, cup36],
+#     [cup37, cup38, cup39, cup40,cup41, cup42, cup43, cup44, cup45],
+#
+# ]
+# from object import cup
+# cup1 = cup('blue' ,0)
+# cup2 = cup('green' ,0)
+# cup3 = cup('Black' ,0)
+# cup4 = cup('Black' ,0)
+# cup5 = cup('Black' ,0)
+# cup6 = cup('Black' ,0)
+# cup7 = cup('Black' ,0)
+# cup8 = cup('Black' ,0)
+# cup9 = cup('Black' ,0)
+# cup10 = cup('wight' ,0)
+# cup11 = cup('wight' ,0)
+# cup12 = cup('Black' ,0)
+# cup13 = cup('wight' ,0)
+# cup14 = cup('wight' ,0)
+# cup15 = cup('wight' ,0)
+# cup16 = cup('Black' ,0)
+# cup17 = cup('Black' ,0)
+# cup18 = cup('red' ,0)
+# cup19 = cup('wight' ,0)
+# cup20 = cup('wight' ,0)
+# cup21 = cup('wight' ,0)
+# cup22 = cup('wight' ,0)
+# cup23 = cup('wight' ,0)
+# cup24 = cup('Black' ,0)
+# cup25 = cup('Black' ,0)
+# cup26 = cup('wight' ,0)
+# cup27 = cup('wight' ,0)
+# cup28 = cup('wight' ,0)
+# cup29 = cup('wight' ,0)
+# cup30 = cup('wight' ,0)
+# cup31 = cup('wight' ,0)
+# cup32 = cup('Black' ,0)
+# cup33 = cup('Black' ,0)
+# cup34 = cup('Black' ,0)
+# cup35 = cup('Black' ,0)
+# cup36 = cup('Black' ,0)
+# cup37 = cup('red' ,1)
+# cup38 = cup('Black' ,0)
+# cup39 = cup('Black' ,0)
+# cup40 = cup('Black' ,0)
+# cup41 = cup('wight' ,0)
+# cup42 = cup('wight' ,0)
+# cup43 = cup('wight' ,0)
+# cup44 = cup('Black' ,0)
+# cup45 = cup('Black' ,0)
+# cup46 = cup('Black' ,0)
+# cup47 = cup('wight' ,0)
+# cup48 = cup('wight' ,0)
+#
+#
+#
+#
+#
+# puzzle = [
+#     [cup1.color,cup2.color,cup3.color,cup4.color,cup5.color,cup6.color,cup7.color,cup8.color],
+#
+#     [cup9.color,cup10.color,cup11.color,cup12.color,cup13.color,cup14.color,cup15.color,cup16.color],
+#
+#     [cup17.color,cup18.color,cup19.color,cup20.color,cup21.color,cup22.color,cup23.color,cup24.color],
+#
+#     [cup25.color,cup26.color,cup27.color,cup28.color,cup29.color,cup30.color,cup31.color,cup32.color],
+#
+#     [cup33.color,cup34.color,cup35.color,cup36.color,cup37.color,cup38.color,cup39.color,cup40.color],
+#
+#     [cup41.color,cup42.color,cup43.color,cup44.color,cup45.color,cup46.color,cup47.color,cup48.color] ,
+# ]
+#
+#
+#
